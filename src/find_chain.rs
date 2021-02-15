5b626fcb8d6a20af0908a7612022d6387108e3c1
nanlei.roy <nanlei.roy@bytedance.com>
2021-02-15 19:13:15 +0800

    implemented

--- src/find_chain.rs
+++ src/find_chain.rs
@@ -5,9 +5,9 @@ use std::collections::HashMap;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[allow(non_snake_case)]
 pub struct Node {
-  Word: String,
-  ID: i32,
-  Next: Vec<i32>,
+  pub Word: String,
+  pub ID: i32,
+  pub Next: Vec<i32>,
 }
 
 pub fn read_graph(data: &str) -> Vec<Node> {
@@ -15,37 +15,49 @@ pub fn read_graph(data: &str) -> Vec<Node> {
   return v;
 }
 
-type IDToNodeMap = HashMap<i32, Node>;
+pub struct MaxLoopCount {
+  pub value: i64,
+}
 
-// MakeWordToGraphItemMap make a word->Graph map
-pub fn make_word_to_node_map(graph: &Vec<Node>) -> IDToNodeMap {
-  let mut result: IDToNodeMap = HashMap::new();
+pub type IDToNextMap = HashMap<i32, Vec<i32>>;
+pub type WordToIDMap = HashMap<String, i32>;
+
+/// make a word->Graph map
+pub fn make_id_to_node_map(graph: &Vec<Node>) -> IDToNextMap {
+  let mut result: IDToNextMap = HashMap::new();
   for node in graph {
-    result.insert(node.ID, node.clone());
+    result.insert(node.ID, node.Next.clone());
   }
   return result;
 }
 
-/// FindLongestChain perform a dfs into Graph to find longest idiom chain
+/// make a word->id map
+pub fn make_word_to_id_map(graph: &Vec<Node>) -> WordToIDMap {
+  let mut result: WordToIDMap = HashMap::new();
+  for node in graph {
+    result.insert(node.Word.clone(), node.ID);
+  }
+  return result;
+}
+
+/// perform a dfs into Graph to find longest idiom chain
 pub fn find_longest_chain(
   id: i32,
   chain: &Vec<i32>,
-  node_map: &IDToNodeMap,
-  max_loop_count: i64,
+  node_map: &IDToNextMap,
+  max_loop_count: &mut MaxLoopCount,
 ) -> Vec<i32> {
-  println!("count: {}", max_loop_count);
-  // max_loop_count = max_loop_count - 1;
-  if max_loop_count < 0 {
-    println!("here");
+  max_loop_count.value = max_loop_count.value - 1;
+  if max_loop_count.value < 0 {
     return chain.clone();
   }
 
-  let next_nodes: Node = node_map.get(&id).unwrap().clone();
+  let next_words = node_map.get(&id).unwrap();
   let mut valid_next_words: Vec<i32> = Vec::new();
 
-  for id in next_nodes.Next {
+  for id in next_words {
     if !chain.contains(&id) {
-      valid_next_words.push(id)
+      valid_next_words.push(*id)
     }
   }
   if valid_next_words.len() == 0 {
@@ -56,9 +68,8 @@ pub fn find_longest_chain(
   for id in valid_next_words {
     let mut added_chain = chain.clone();
     added_chain.push(id);
-    let current_chain = find_longest_chain(id, &added_chain, node_map, max_loop_count - 1);
+    let current_chain = find_longest_chain(id, &added_chain, node_map, max_loop_count);
     let current_length = current_chain.len() as i32;
-    println!("current_length: {}", current_length);
     if current_length > max_length {
       max_length = current_length;
       longest_chain = current_chain;
@@ -66,4 +77,18 @@ pub fn find_longest_chain(
   }
 
   return longest_chain;
+}
+
+pub fn map_id_to_word(graph: &Vec<Node>, id_list: &Vec<i32>) -> Vec<String> {
+  let mut id_to_word_map: HashMap<i32, String> = HashMap::new();
+  for node in graph {
+    id_to_word_map.insert(node.ID, node.Word.clone());
+  }
+
+  let mut result: Vec<String> = Vec::new();
+  for id in id_list {
+    let word: String = id_to_word_map.get(&id).unwrap().to_string();
+    result.push(word);
+  }
+  return result;
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,16 @@ use db::DATA;
 
 fn main() {
   let graph = read_graph(DATA);
-
-  let word_to_node_map = make_word_to_node_map(&graph);
-  let res = find_longest_chain(1, &vec![1], &word_to_node_map, 1000);
-  println!("res: {:?}", res);
+  let word_to_id_map: WordToIDMap = make_word_to_id_map(&graph);
+  let id: i32 = word_to_id_map.get("文武双全").unwrap().clone();
+  let id_to_next_map = make_id_to_node_map(&graph);
+  let res: Vec<i32> = find_longest_chain(
+    id,
+    &vec![id],
+    &id_to_next_map,
+    &mut MaxLoopCount { value: 100_000_000 },
+  );
+  let words = map_id_to_word(&graph, &res);
+  println!("words: {:?}", words);
+  println!("length: {}", words.len());
 }
