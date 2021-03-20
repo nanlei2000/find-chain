434ebb3bc5fcbcdc7b421344e31c578769a002b4
nanlei <36439285+nanlei2000@users.noreply.github.com>
2021-03-20 22:24:15 +0800

    Lifetime (#2)
    * add lifetime tags
    
    * add break
    
    * remove reference for primitive type
    
    * remove needless clone
    
    * add hashset back
    
    * replace refcell to &mut
    
    Co-authored-by: nanlei.roy <nanlei.roy@bytedance.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## find chain
+
 - 包和模块: https://wiki.jikexueyuan.com/project/rust-primer/module/module.html
 - https://deterministic.space/high-performance-rust.html
 - Longest path problem: https://en.wikipedia.org/wiki/Longest_path_problem#cite_note-10
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod find_chain;
 use find_chain::*;
 mod db;
 use db::DATA;
-use std::cell::RefCell;
 use std::collections::HashSet;
 use std::env;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -14,7 +13,7 @@ fn main() {
   // if args.len() != 3 {
   //   panic!("use like this: cmd 10000 文武双全")
   // }
-  let max_loop_count = &args[1].parse::<i64>().unwrap();
+  let mut max_loop_count = args[1].parse::<i64>().unwrap().to_owned();
   let word = &args[2];
   println!(
     "[info] head idiom: {}, max loop count: {}",
@@ -39,13 +38,7 @@ fn main() {
     let now = timestamp();
     let mut set: HashSet<u16> = HashSet::new();
     set.insert(id);
-    let res: Vec<u16> = find_longest_chain(
-      id,
-      set,
-      vec![id],
-      &id_to_next_map,
-      &RefCell::new(*max_loop_count),
-    );
+    let res: Vec<u16> = find_longest_chain(id, set, vec![id], &id_to_next_map, &mut max_loop_count);
     let duration = timestamp() - now;
     let words = map_id_to_word(&graph, &res);
     println!("chain: {:?}", words);
